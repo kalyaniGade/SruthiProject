2712c48393194fc0d798efc01a43b3d6df276c22
C-PN15 <C-PN15@DAL-8CO097-9W7.wks.tsh.thomson.com>
2019-06-13 16:55:05 +0530

    Eight Commit

--- src/main/java/Practice/Testprogram.java
+++ src/main/java/Practice/Testprogram.java
@@ -5,6 +5,8 @@ public class Testprogram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Sonarqube to find bugs");
+		
+		System.out.println("Check poll scm");
 
 	}
 
